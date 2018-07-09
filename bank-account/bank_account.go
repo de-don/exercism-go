127c8ebee9e4cd5935b0315d3cd37ca1a5da81a5
de-don <de-don@mail.ru>
2018-07-09 15:44:28 +0700

    solved "bank_account"

---
+++ bank-account/bank_account.go
@@ -0,0 +1,57 @@
+package account
+
+type Bank struct {
+	balance int
+	open    bool
+	ch      *chan bool
+}
+
+func Open(sum int) *Bank {
+	if sum < 0 {
+		return nil
+	}
+	ch := make(chan bool, 1)
+	bank := Bank{balance: sum, open: true, ch: &ch}
+	bank.Unlock()
+
+	return &bank
+}
+
+// lock bank using chan
+func (b *Bank) Lock() {
+	<-*b.ch
+}
+
+// unlock bank using chan
+func (b *Bank) Unlock() {
+	*b.ch <- true
+}
+
+func (b Bank) Balance() (int, bool) {
+	if b.open {
+		return b.balance, true
+	}
+	return 0, false
+}
+
+func (b *Bank) Close() (int, bool) {
+	b.Lock()
+	defer b.Unlock()
+
+	if !b.open {
+		return 0, false
+	}
+	b.open = false
+	return b.balance, true
+}
+
+func (b *Bank) Deposit(sum int) (int, bool) {
+	b.Lock()
+	defer b.Unlock()
+
+	if (!b.open) || (b.balance+sum < 0) {
+		return 0, false
+	}
+	b.balance += sum
+	return b.balance, true
+}
